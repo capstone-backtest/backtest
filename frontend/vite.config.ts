3b39100fcded50585173c51f104b0cabd02be63c
kyj0503 <heroria0503@gmail.com>
2025-08-22 06:12:58 +0900

    개발 도커 환경에 맞춰 프록시 설정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,15 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+const target = process.env.API_PROXY_TARGET || 'http://localhost:8000'
+
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target,
         changeOrigin: true,
       }
     }
   }
-}) 
+})
